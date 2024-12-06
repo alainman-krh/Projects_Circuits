51a4e68ab6e28107d532351b74f1fb29cbfe9736
Alain <alainman.krh@gmail.com>
2024-12-05 22:42:17 -0500

    Start using pydrv_install.toml.

--- PCMediaRemote/pkg_install/1-PkgUpload/pkg_upload.py
+++ PCMediaRemote/pkg_install/1-PkgUpload/pkg_upload.py
@@ -1,14 +1,13 @@
 #pkg_upload.py: Upload project code to CircuitPython board
 #-------------------------------------------------------------------------------
-from UploadTools import UploadProj
+from UploadTools import UploadPkg
 from os.path import join as joinpath
 
 #User config
 #-------------------------------------------------------------------------------
 DEST_DRIVE = "E:\\"
-proj = "MediaRemote_RP2040"
+pkg = "MediaRemote_RP2040"
 
-proj = joinpath("pkg_install", proj)
-UploadProj(proj, DEST_DRIVE, refresh_libs=True)
+UploadPkg(pkg, DEST_DRIVE, refresh_libs=True)
 
 #Last line
