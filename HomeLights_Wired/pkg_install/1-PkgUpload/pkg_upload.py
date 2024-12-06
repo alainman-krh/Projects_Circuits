58657f748b7d63434658b5ea96c0d7dc82e30a6f
Alain <alainman.krh@gmail.com>
2024-12-05 22:50:47 -0500

    Switch over to pydrv_install.toml

--- HomeLights_Wired/pkg_install/1-PkgUpload/pkg_upload.py
+++ HomeLights_Wired/pkg_install/1-PkgUpload/pkg_upload.py
@@ -1,16 +1,15 @@
 #pkg_upload.py: Upload project code to CircuitPython board
 #-------------------------------------------------------------------------------
-from UploadTools import UploadProj
+from UploadTools import UploadPkg
 from os.path import join as joinpath
 
 #User config
 #-------------------------------------------------------------------------------
 DEST_DRIVE = "E:\\"
-proj = "LightCtrl3Boards_2040pico"
-#proj = "LightCtrl3Boards_AFMacropad"
-#proj = "LightCtrl3Boards_CPbluefruit"
+pkg = "LightCtrl3Boards_2040pico"
+#pkg = "LightCtrl3Boards_AFMacropad"
+#pkg = "LightCtrl3Boards_CPbluefruit"
 
-proj = joinpath("pkg_install", proj)
-UploadProj(proj, DEST_DRIVE, refresh_libs=True)
+UploadPkg(pkg, DEST_DRIVE, refresh_libs=True)
 
 #Last line
